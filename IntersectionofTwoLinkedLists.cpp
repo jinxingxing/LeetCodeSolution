ecdd9b2213439da52656fbdb43dee95a403f3191
JinXing <jxinging@gmail.com>
2014-12-25 22:45:32 +0800

    Update IntersectionofTwoLinkedLists.cpp

--- IntersectionofTwoLinkedLists.cpp
+++ IntersectionofTwoLinkedLists.cpp
@@ -14,10 +14,10 @@ begin to intersect at node c1.
 
 Notes:
 
-If the two linked lists have no intersection at all, return null.
-The linked lists must retain their original structure after the function returns.
-You may assume there are no cycles anywhere in the entire linked structure.
-Your code should preferably run in O(n) time and use only O(1) memory.
+- If the two linked lists have no intersection at all, return null.
+- The linked lists must retain their original structure after the function returns.
+- You may assume there are no cycles anywhere in the entire linked structure.
+- Your code should preferably run in O(n) time and use only O(1) memory.
 
 
 
